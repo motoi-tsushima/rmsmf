c34c5e75bb69e8ed5b08e294e5e6c0709491b9cb
motoi-tsushima <motoi.tsushima@gmail.com>
2026-02-07 13:59:51 +0900

    一部のヘルプメッセージの不備を修正しました。

--- txprobe/Properties/AssemblyInfo.cs
+++ txprobe/Properties/AssemblyInfo.cs
@@ -30,8 +30,8 @@ using System.Runtime.InteropServices;
 //      ビルド番号
 //      リビジョン
 //
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.0.4.1")]
+[assembly: AssemblyFileVersion("1.0.4.1")]
 [assembly: NeutralResourcesLanguage("ja")]
 
 // テストアセンブリからinternalメンバーへのアクセスを許可
